b9f177ce6eb0a7df79b6ad5b7904125dc23f034a
Chmichev Boris <boris362@yandex.ru>
2017-09-09 15:17:15 +0300

    Restrictions added

--- tests/vk-streaming.test.js
+++ tests/vk-streaming.test.js
@@ -4,7 +4,7 @@ const test = require('ava')
   , vkStreaming = require('../lib/vk-streaming')
 
 test('Should authenticate via access token and retrieve an endpoint and key', async t => {
-  const res = await vkStreaming._authWithToken(process.env['VK_ACCESS_TOKEN'])
+  const res = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
   res.key && res.endpoint
     ? t.pass()
     : t.fail()
@@ -12,7 +12,7 @@ test('Should authenticate via access token and retrieve an endpoint and key', as
 
 test('Should reject with an error when access token is invalid', async t => {
   try {
-    await vkStreaming._authWithToken('poor_token_💩')
+    await vkStreaming.authWithToken('poor_token_💩')
   } catch (err) {
     t.pass()
   }

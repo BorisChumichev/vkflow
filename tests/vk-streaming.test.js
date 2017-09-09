c315f2363f2546f43c81a7477837a5efa9d90a3f
Chmichev Boris <boris362@yandex.ru>
2017-09-09 18:37:52 +0300

    CRUDs for rules

--- tests/vk-streaming.test.js
+++ tests/vk-streaming.test.js
@@ -1,19 +1,74 @@
 require('dotenv').config()
 
 const test = require('ava')
+  , { has } = require('ramda')
   , vkStreaming = require('../lib/vk-streaming')
 
-test('Should authenticate via access token and retrieve an endpoint and key', async t => {
-  const res = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
-  res.key && res.endpoint
+/*
+  Flushing all the rules for the stream before
+  we run any tests, as well as cleaning the ones
+  that created during testing
+*/
+test.before(vkStreaming.flushRules)
+test.after(vkStreaming.flushRules)
+
+const sampleRule = () =>
+  ({ rule:
+    { value: Math.random().toString(32).slice(2)
+    , tag: Math.random().toString(32).slice(10)
+    }
+  })
+
+test('Authenticates via access token and retrieves an endpoint and key', async t => {
+  const { endpoint, key } = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
+  key && endpoint
     ? t.pass()
     : t.fail()
 })
 
-test('Should reject with an error when access token is invalid', async t => {
+test('Rejects with an error when access token is invalid', async t => {
   try {
     await vkStreaming.authWithToken('poor_token_💩')
   } catch (err) {
     t.pass()
   }
+})
+
+test('Fetches stream rules', async t => {
+  const { endpoint, key } = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
+  const response = await vkStreaming.getRules(endpoint, key)
+
+  t.true(response.code === 200)
+  t.true(has('rules', response))
+  t.pass()
+})
+
+test('Propagates an error if it occures', async t => {
+  const rule = sampleRule()
+  const { endpoint, key } = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
+
+  try {
+    while (true) await vkStreaming.postRule(endpoint, key, rule)
+  } catch(err) {
+    t.true(err.error_code === 2001)
+    t.pass()
+  }
+  
+})
+
+test('Adds rules', async t => {
+  const { endpoint, key } = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
+  const response = await vkStreaming.postRule(endpoint, key, sampleRule())
+
+  t.true(response.code === 200)
+  t.pass()
+})
+
+test('Deletes the rules', async t => {
+  const rule = sampleRule()
+  const { endpoint, key } = await vkStreaming.authWithToken(process.env['VK_ACCESS_TOKEN'])
+  const response = await vkStreaming.postRule(endpoint, key, rule)
+
+  t.true(response.code === 200)
+  t.pass()
 })

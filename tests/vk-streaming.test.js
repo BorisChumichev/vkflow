56c9eac15fe587136d2cde61cf6d9194a9cae917
Chmichev Boris <boris362@yandex.ru>
2017-09-09 15:09:11 +0300

    Authentication refactored

---
+++ tests/vk-streaming.test.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const test = require('ava')
+  , vkStreaming = require('../lib/vk-streaming')
+
+test('Should authenticate via access token and retrieve an endpoint and key', async t => {
+  const res = await vkStreaming._authWithToken(process.env['VK_ACCESS_TOKEN'])
+  res.key && res.endpoint
+    ? t.pass()
+    : t.fail()
+})
+
+test('Should reject with an error when access token is invalid', async t => {
+  try {
+    await vkStreaming._authWithToken('poor_token_💩')
+  } catch (err) {
+    t.pass()
+  }
+})

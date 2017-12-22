19c0d7ff5444b90745bc24b01af6e19d2bb688bf
Chmichev Boris <boris362@yandex.ru>
2017-12-22 18:03:42 +0300

    Get rid of run.js

--- run.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const auth = require('./lib/auth')
-
-auth(process.env['VK_ACCESS_TOKEN'])
-  .then(res => console.log(res))
-  // configure rp-vk with { endpoint, key } passed by previous method

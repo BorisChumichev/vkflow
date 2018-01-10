6fcde9ff801d71a195ebb1ce353e7421018d1b7a
Chmichev Boris <boris362@yandex.ru>
2018-01-10 14:55:18 +0300

    initial

--- index.js
+++
@@ -1,8 +0,0 @@
-const vkflow = require('./lib/factory')
-  , WSClient = require('./lib/ws-client')
-  , HTTPClient = require('./lib/http-client')
-
-vkflow.VKWebSocket = WSClient
-vkflow.VKStreamingAPI = HTTPClient
-
-module.exports = vkflow
